b56b5a8920ba7026f6ea6c3c3ce18293acd37725
Andrei Milto <61744578+andreimilto@users.noreply.github.com>
2021-02-04 00:17:46 +0300

    Add .NET 5 target, optimize and refactor the code
    * Add .NET 5 as a target platform for the projects HkdfStandard
      and HkdfStandard.Test
    
    * HkdfStandard project:
        - refactor and optimize the code of the Hkdf class
        - disable compiler optimizations for the memory cleaning operations

--- src/HkdfStandard.Test/HkdfTest.cs
+++ src/HkdfStandard.Test/HkdfTest.cs
@@ -269,7 +269,7 @@ namespace HkdfStandard.Test
         #endregion
 
 
-#if NETCOREAPP3_1
+#if (NETCOREAPP3_1 || NET5_0)
 
         #region Extract Span<byte>
 
--- src/HkdfStandard/Hkdf.cs
+++ src/HkdfStandard/Hkdf.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
 namespace HkdfStandard
@@ -32,10 +33,10 @@ namespace HkdfStandard
         /// <returns>The pseudorandom key of the size of hash algorithm output, i.e.: MD5 - 16 bytes, SHA1 - 20 bytes, SHA256 - 32 bytes, SHA384 - 48 bytes, SHA512 - 64 bytes.</returns>
         /// <exception cref="ArgumentNullException">The argument <paramref name="ikm"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentOutOfRangeException">The hash algorithm specified in the parameter <paramref name="hashAlgorithmName"/> is not supported.</exception>
-#if NETSTANDARD2_1
-        public static byte[] Extract(HashAlgorithmName hashAlgorithmName, byte[] ikm, byte[]? salt = null)
-#else
+#if NETSTANDARD1_3
         public static byte[] Extract(HashAlgorithmName hashAlgorithmName, byte[] ikm, byte[] salt = null)
+#elif (NETSTANDARD2_1 || NET5_0)
+        public static byte[] Extract(HashAlgorithmName hashAlgorithmName, byte[] ikm, byte[]? salt = null)
 #endif
         {
             if (!IsHashAlgorithmSupported(hashAlgorithmName))
@@ -46,7 +47,7 @@ namespace HkdfStandard
             return PerformExtraction(hashAlgorithmName, ikm, salt);
         }
 
-#if NETSTANDARD2_1
+#if (NETSTANDARD2_1 || NET5_0)
         /// <summary>
         /// Extracts a pseudorandom key from the provided input key material using the specified salt value.
         /// </summary>
@@ -64,7 +65,7 @@ namespace HkdfStandard
             if (prk.Length < hashOutputLength)
                 throw new ArgumentException($"The supplied pseudorandom key buffer is too small. It must be large enough to accommodate the extracted pseudorandom key, that is, at least {hashOutputLength} bytes in case of {hashAlgorithmName}.", nameof(prk));
 
-            return PerformExtraction(hashAlgorithmName, hashOutputLength, ikm, salt, prk);
+            return PerformExtraction(hashAlgorithmName, ikm, salt, prk);
         }
 #endif
 
@@ -80,10 +81,10 @@ namespace HkdfStandard
         /// <exception cref="ArgumentNullException">The argument <paramref name="prk"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentOutOfRangeException">The hash algorithm specified in the parameter <paramref name="hashAlgorithmName"/> is not supported or the value of the parameter <paramref name="outputLength"/> is invalid (either too small or too large).</exception>
         /// <exception cref="ArgumentException">The length of the <paramref name="prk"/> is less than the length of the output of the hash algorithm.</exception>
-#if NETSTANDARD2_1
-        public static byte[] Expand(HashAlgorithmName hashAlgorithmName, byte[] prk, int outputLength, byte[]? info = null)
-#else
+#if NETSTANDARD1_3
         public static byte[] Expand(HashAlgorithmName hashAlgorithmName, byte[] prk, int outputLength, byte[] info = null)
+#elif (NETSTANDARD2_1 || NET5_0)
+        public static byte[] Expand(HashAlgorithmName hashAlgorithmName, byte[] prk, int outputLength, byte[]? info = null)
 #endif
         {
             if (!TryGetHashOutputLength(hashAlgorithmName, out int hashOutputLength))
@@ -100,7 +101,7 @@ namespace HkdfStandard
             return PerformExpansion(hashAlgorithmName, hashOutputLength, prk, info, outputLength);
         }
 
-#if NETSTANDARD2_1
+#if (NETSTANDARD2_1 || NET5_0)
         /// <summary>
         /// Expands the provided pseudorandom key into an output keying material of the desired length using optional context information.
         /// </summary>
@@ -140,10 +141,10 @@ namespace HkdfStandard
         /// <returns>The output keying material.</returns>
         /// <exception cref="ArgumentNullException">The argument <paramref name="ikm"/> is <c>null</c>.</exception>
         /// <exception cref="ArgumentOutOfRangeException">The hash algorithm specified in the parameter <paramref name="hashAlgorithmName"/> is not supported or the value of the parameter <paramref name="outputLength"/> is invalid (either too small or too large).</exception>
-#if NETSTANDARD2_1
-        public static byte[] DeriveKey(HashAlgorithmName hashAlgorithmName, byte[] ikm, int outputLength, byte[]? salt = null, byte[]? info = null)
-#else
+#if NETSTANDARD1_3
         public static byte[] DeriveKey(HashAlgorithmName hashAlgorithmName, byte[] ikm, int outputLength, byte[] salt = null, byte[] info = null)
+#elif (NETSTANDARD2_1 || NET5_0)
+        public static byte[] DeriveKey(HashAlgorithmName hashAlgorithmName, byte[] ikm, int outputLength, byte[]? salt = null, byte[]? info = null)
 #endif
         {
             if (!TryGetHashOutputLength(hashAlgorithmName, out int hashOutputLength))
@@ -155,20 +156,10 @@ namespace HkdfStandard
             if (outputLength > hashOutputLength * maxOutputLengthCoef)
                 throw new ArgumentOutOfRangeException(nameof(outputLength), $"The specified output length is too large. The maximum size of the output is {maxOutputLengthCoef} times the hash-output-length, i.e. {hashOutputLength * maxOutputLengthCoef} bytes in case of {hashAlgorithmName}.");
 
-            byte[] prk = PerformExtraction(hashAlgorithmName, ikm, salt);
-            byte[] okm;
-            try
-            {
-                okm = PerformExpansion(hashAlgorithmName, hashOutputLength, prk, info, outputLength);
-            }
-            finally
-            {
-                ClearArray(prk);
-            }
-            return okm;
+            return PerformKeyDerivation(hashAlgorithmName, hashOutputLength, ikm, salt, info, outputLength);
         }
 
-#if NETSTANDARD2_1
+#if (NETSTANDARD2_1 || NET5_0)
         /// <summary>
         /// Derives the output keying material of the desired length from the input key material using the provided salt and context information.
         /// </summary>
@@ -191,25 +182,16 @@ namespace HkdfStandard
             if (output.Length > hashOutputLength * maxOutputLengthCoef)
                 throw new ArgumentException($"The supplied output buffer is too large. The maximum size of the output buffer is {maxOutputLengthCoef} times the hash-output-length, i.e. {hashOutputLength * maxOutputLengthCoef} bytes in case of {hashAlgorithmName}.", nameof(output));
 
-            Span<byte> prk = stackalloc byte[hashOutputLength];
-            try
-            {
-                PerformExtraction(hashAlgorithmName, hashOutputLength, ikm, salt, prk);
-                PerformExpansion(hashAlgorithmName, hashOutputLength, prk, info, output);
-            }
-            finally
-            {
-                prk.Clear();
-            }
+            PerformKeyDerivation(hashAlgorithmName, hashOutputLength, ikm, salt, info, output);
         }
 #endif
 
 
 
-#if NETSTANDARD2_1
-        private static byte[] PerformExtraction(HashAlgorithmName hashAlgorithmName, byte[] ikm, byte[]? salt)
-#else
+#if NETSTANDARD1_3
         private static byte[] PerformExtraction(HashAlgorithmName hashAlgorithmName, byte[] ikm, byte[] salt)
+#elif (NETSTANDARD2_1 || NET5_0)
+        private static byte[] PerformExtraction(HashAlgorithmName hashAlgorithmName, byte[] ikm, byte[]? salt)
 #endif
         {
             // RFC 5869 section 2.2 states that if the salt is not provided,
@@ -233,16 +215,17 @@ namespace HkdfStandard
         }
 
 #if NETSTANDARD2_1
-        private static unsafe int PerformExtraction(HashAlgorithmName hashAlgorithmName, int hashOutputLength, ReadOnlySpan<byte> ikm, ReadOnlySpan<byte> salt, Span<byte> prk)
+        private static unsafe int PerformExtraction(HashAlgorithmName hashAlgorithmName, ReadOnlySpan<byte> ikm, ReadOnlySpan<byte> salt, Span<byte> prk)
         {
             int bytesExtracted;
 
             byte[] saltBytes = new byte[salt.Length];
             fixed (byte* saltBytesPointer = saltBytes)
             {
-                salt.CopyTo(saltBytes);
                 try
                 {
+                    salt.CopyTo(saltBytes);
+
                     // Using incremental HMAC because it is faster than non-incremental.
 
                     using (var hmac = IncrementalHash.CreateHMAC(hashAlgorithmName, saltBytes))
@@ -258,8 +241,26 @@ namespace HkdfStandard
                 }
                 finally
                 {
-                    ClearArray(saltBytes);
+                    Clear(saltBytes);
                 }
+            }
+
+            return bytesExtracted;
+        }
+#elif NET5_0
+        private static int PerformExtraction(HashAlgorithmName hashAlgorithmName, ReadOnlySpan<byte> ikm, ReadOnlySpan<byte> salt, Span<byte> prk)
+        {
+            int bytesExtracted;
+
+            using (var hmac = IncrementalHash.CreateHMAC(hashAlgorithmName, salt))
+            {
+                hmac.AppendData(ikm);
+
+                // The method is supposed to always return true, because the destination buffer will always be large enough to accommodate the HMAC value (https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.incrementalhash.trygethashandreset?view=net-5.0).
+                // However, we still check the returned value just to be on the safe side.
+
+                if (!hmac.TryGetHashAndReset(prk, out bytesExtracted))
+                    throw new CryptographicException($"Failed to compute the MAC during the Extract stage of HKDF.");
             }
 
             return bytesExtracted;
@@ -267,11 +268,8 @@ namespace HkdfStandard
 #endif
 
 
-#if NETSTANDARD2_1
-        private static byte[] PerformExpansion(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, byte[] prk, byte[]? info, int outputLength)
-#else
+#if NETSTANDARD1_3
         private static byte[] PerformExpansion(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, byte[] prk, byte[] info, int outputLength)
-#endif
         {
             if (info == null)
                 info = Array.Empty<byte>();
@@ -293,23 +291,41 @@ namespace HkdfStandard
                     int blockLength = Math.Min(hmacOutputLength, outputLength - blocktOffset);
                     Buffer.BlockCopy(currentBlock, 0, result, blocktOffset, blockLength);
 
-                    ClearArray(previousBlock);
+                    Clear(previousBlock);
                     previousBlock = currentBlock;
                 }
             }
             catch
             {
-                ClearArray(previousBlock);
+                Clear(previousBlock);
+                Clear(result);
                 throw;
             }
             finally
             {
                 hmac.Dispose();
-                ClearArray(currentBlock);
+                Clear(currentBlock);
             }
 
             return result;
         }
+#elif (NETSTANDARD2_1 || NET5_0)
+        private static byte[] PerformExpansion(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, byte[] prk, byte[]? info, int outputLength)
+        {
+            byte[] output = new byte[outputLength];
+            try
+            {
+                PerformExpansion(hashAlgorithmName, hmacOutputLength, prk, info, output);
+            }
+            catch
+            {
+                Clear(output);
+                throw;
+            }
+            
+            return output;
+        }
+#endif
 
 #if NETSTANDARD2_1
         private static unsafe void PerformExpansion(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, ReadOnlySpan<byte> prk, ReadOnlySpan<byte> info, Span<byte> output)
@@ -319,9 +335,10 @@ namespace HkdfStandard
             byte[] prkBytes = new byte[prk.Length];
             fixed (byte* prkBytesPointer = prkBytes)
             {
-                prk.CopyTo(prkBytes);
                 try
                 {
+                    prk.CopyTo(prkBytes);
+
                     using var hmac = IncrementalHash.CreateHMAC(hashAlgorithmName, prkBytes);
 
                     int wholeBlockCount = output.Length / hmacOutputLength;
@@ -347,14 +364,98 @@ namespace HkdfStandard
                         }
                         finally
                         {
-                            partialBlock.Clear();
+                            Clear(partialBlock);
                         }
                     }
                 }
                 finally
                 {
-                    ClearArray(prkBytes);
+                    Clear(prkBytes);
                 }
+            }
+        }
+#elif NET5_0
+        private static void PerformExpansion(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, ReadOnlySpan<byte> prk, ReadOnlySpan<byte> info, Span<byte> output)
+        {
+            Span<byte> counter = stackalloc byte[1];
+            Span<byte> previousBlock = Span<byte>.Empty;
+
+            using (var hmac = IncrementalHash.CreateHMAC(hashAlgorithmName, prk))
+            {
+                int wholeBlockCount = output.Length / hmacOutputLength;
+                for (int i = 1; i <= wholeBlockCount; i++)
+                {
+                    counter[0] = (byte)i;
+                    var currentBlock = output.Slice((i - 1) * hmacOutputLength, hmacOutputLength);
+                    GenerateOutputBlock(hmac, previousBlock, info, counter, currentBlock);
+                    previousBlock = currentBlock;
+                }
+
+                int bytesWritten = wholeBlockCount * hmacOutputLength;
+                int bytesLeft = output.Length - bytesWritten;
+                if (bytesLeft > 0)
+                {
+                    counter[0]++;
+                    Span<byte> partialBlock = stackalloc byte[hmacOutputLength];
+                    try
+                    {
+                        GenerateOutputBlock(hmac, previousBlock, info, counter, partialBlock);
+                        partialBlock.Slice(0, bytesLeft)
+                                    .CopyTo(output.Slice(bytesWritten));
+                    }
+                    finally
+                    {
+                        Clear(partialBlock);
+                    }
+                }
+            }
+        }
+#endif
+
+
+#if NETSTANDARD1_3
+        private static byte[] PerformKeyDerivation(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, byte[] ikm, byte[] salt, byte[] info, int outputLength)
+        {
+            byte[] prk = PerformExtraction(hashAlgorithmName, ikm, salt);
+            try
+            {
+                return PerformExpansion(hashAlgorithmName, hmacOutputLength, prk, info, outputLength);
+            }
+            finally
+            {
+                Clear(prk);
+            }
+        }
+#elif (NETSTANDARD2_1 || NET5_0)
+        private static byte[] PerformKeyDerivation(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, byte[] ikm, byte[]? salt, byte[]? info, int outputLength)
+        {
+            byte[] output = new byte[outputLength];
+            try
+            {
+                PerformKeyDerivation(hashAlgorithmName, hmacOutputLength, ikm, salt, info, output);
+            }
+            catch
+            {
+                Clear(output);
+                throw;
+            }
+            
+            return output;
+        }
+#endif
+
+#if (NETSTANDARD2_1 || NET5_0)
+        private static void PerformKeyDerivation(HashAlgorithmName hashAlgorithmName, int hmacOutputLength, ReadOnlySpan<byte> ikm, ReadOnlySpan<byte> salt, ReadOnlySpan<byte> info, Span<byte> output)
+        {
+            Span<byte> prk = stackalloc byte[hmacOutputLength];
+            try
+            {
+                PerformExtraction(hashAlgorithmName, ikm, salt, prk);
+                PerformExpansion(hashAlgorithmName, hmacOutputLength, prk, info, output);
+            }
+            finally
+            {
+                Clear(prk);
             }
         }
 #endif
@@ -426,7 +527,7 @@ namespace HkdfStandard
             return hmac.GetHashAndReset();
         }
 
-#if NETSTANDARD2_1
+#if (NETSTANDARD2_1 || NET5_0)
         private static void GenerateOutputBlock(IncrementalHash hmac, ReadOnlySpan<byte> previousOutputBlock, ReadOnlySpan<byte> info, ReadOnlySpan<byte> counter, Span<byte> outputBlock)
         {
             hmac.AppendData(previousOutputBlock);
@@ -441,9 +542,20 @@ namespace HkdfStandard
         }
 #endif
         
-        private static void ClearArray(byte[] array)
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static void Clear(byte[] buffer)
         {
-            Array.Clear(array, 0, array.Length);
+            Array.Clear(buffer, 0, buffer.Length);
         }
+
+#if (NETSTANDARD2_1 || NET5_0)
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static void Clear(Span<byte> buffer)
+        {
+            buffer.Clear();
+        }
+#endif
+
     }
 }
